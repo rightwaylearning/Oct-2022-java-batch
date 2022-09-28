e6aae142ae3d3756b33c4207e2dc5d798bcdd79a
Ajit_Fargade <ajitfargade07@gmail.com>
2022-09-28 23:25:16 +0530

    ajit new package

--- java-basic-programming-ch-1/src/ajit_fargade/ajitfargade.java
+++ java-basic-programming-ch-1/src/ajit_fargade/ajitfargade.java
@@ -1,7 +1,7 @@
-package ajit.fargade;
+package ajit_fargade;
 
 public class ajitfargade {
 	public static void main(String[] args) {
-		System.out.println("Ajit fragade");
+		System.out.println("Ajit Fargade");
 	}
 }
