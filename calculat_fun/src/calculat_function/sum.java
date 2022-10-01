caa7e2f4697de4d416ab0fea9cbd4bc12877faaa
Archana khaire <archanakhaire1986@gmail.com>
2022-09-30 21:46:17 -0400

    basic pro

---
+++ calculat_fun/src/calculat_function/sum.java
@@ -0,0 +1,11 @@
+package calculat_function;
+
+public class sum {
+	public static void main(String[] args ) {
+	int a,b,c=0;
+	a=345;b=275;
+	c=a+b;
+	System.out.println("The sum of two number="+c);
+}
+
+}
---
+++ return1_type/src/module-info.java
@@ -0,0 +1,2 @@
+module return1_type {
+}
