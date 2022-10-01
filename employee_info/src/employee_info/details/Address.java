62cca75618bcf45e9360c621c527a7ef1071e767
sachin lagad <sachin.lagad@gmail.com>
2022-10-01 06:52:31 +0530

    delete all projects almost

--- employee_info/src/employee_info/details/Address.java
+++
@@ -1,9 +0,0 @@
-package employee_info.details;
-
-public class Address {
-
-	String line1;
-	String line2;
-	String landmark;
-	int pin;
-}
---
+++ java-basic-programming-ch-1/src/right/way/learning/RulesToCreateIdentifiers.java
@@ -0,0 +1,16 @@
+package right.way.learning;
+
+public class RulesToCreateIdentifiers {
+
+	public static void main(String[] args) {
+		
+		// valid character set =   [A-Z][a-z][0-9][$,_]
+		// 1] u can create any combination
+		// 2] don't start using digit 
+		
+		int a = 10;
+		int $_____$=50;
+		String first_Name = "sdjfht83756834bMNCbzk";
+		int Nam1e = 20;
+	}
+}
