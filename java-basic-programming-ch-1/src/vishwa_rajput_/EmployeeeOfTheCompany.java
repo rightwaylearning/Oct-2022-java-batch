fee4dab5c7b3e9c9183ea0abbc5f7d82baf9e585
vishwa.rajput <vishwajeetgirase143@gmail.com>
2022-10-13 23:10:06 +0530

    abc

--- java-basic-programming-ch-1/src/vishwa_rajput_/EmployeeeOfTheCompany.java
+++ java-basic-programming-ch-1/src/vishwa_rajput_/EmployeeeOfTheCompany.java
@@ -11,7 +11,7 @@ public class EmployeeeOfTheCompany {
 		public static void main(String[] args) {
 			
 			
-			EmployeeeOfTheCompany[] e = new EmployeeeOfTheCompany[3];
+			//EmployeeeOfTheCompany[] e = new EmployeeeOfTheCompany[3];
 			
 			EmployeeeOfTheCompany x = new EmployeeeOfTheCompany();
 			           
@@ -20,7 +20,7 @@ public class EmployeeeOfTheCompany {
 			                        x.age = 10;
 			                        x.salary ="50k";
 			                        x.cabin ='D';
-			        e[0] = x;      
+			      //  e[0] = x;      
 			
 			        EmployeeeOfTheCompany y = new EmployeeeOfTheCompany();
 			     		           
@@ -30,7 +30,7 @@ public class EmployeeeOfTheCompany {
 			                        y.salary ="55k";
 			                        y.cabin = 'A';
 			              
-			        e[1] = y;
+			      //  e[1] = y;
 			        
 			        EmployeeeOfTheCompany z = new EmployeeeOfTheCompany();
 			           
@@ -40,40 +40,48 @@ public class EmployeeeOfTheCompany {
 	                z.salary ="60k";
 	                z.cabin = 'B';
 	                
-	               e[2] = z;
-			                
-	                               System.out.println(e[0].fname);
-	                               System.out.println(e[0].lname);
-	                               System.out.println(e[0].age);
-	                               System.out.println(e[0].salary);
-	                               System.out.println(e[0].cabin);
+	             //  e[2] = z;
+			               
+	               
+	               
+	               EmployeeeOfTheCompany[] e = {x,y,z};
+	               
+	               	               
+	               	               
+	                             //  System.out.println(e[0].fname);
+	                             //  System.out.println(e[0].lname);
+	                            //   System.out.println(e[0].age);
+	                             //  System.out.println(e[0].salary);
+	                             //  System.out.println(e[0].cabin);
 	                               
 	                               
 	                               
-	                               System.out.println(e[1].fname);
-	                               System.out.println(e[1].lname);
-	                               System.out.println(e[1].age);
-	                               System.out.println(e[1].salary);
-	                               System.out.println(e[1].cabin);
+	                            //   System.out.println(e[1].fname);
+	                            //   System.out.println(e[1].lname);
+	                            //   System.out.println(e[1].age);
+	                           //    System.out.println(e[1].salary);
+	                           //    System.out.println(e[1].cabin);
 	                               
 	                               
-	                               System.out.println(e[2].fname);
-	                               System.out.println(e[2].lname);
-	                               System.out.println(e[2].age);
-	                               System.out.println(e[2].salary);
-	                               System.out.println(e[2].cabin);
-	                               
-	                               
-	                               
-	                               
-	                               
+	                            //   System.out.println(e[2].fname);
+	                            //   System.out.println(e[2].lname);
+	                            //   System.out.println(e[2].age);
+	                           //    System.out.println(e[2].salary);
+	                            //   System.out.println(e[2].cabin);	                               
+	               	            	  	    	   	            	   
+	               
+	               for(int i =0; i<e.length;i++){
+	       			
+	       			System.out.println(e[i].fname);
+	       			System.out.println(e[i].lname);
+	       			System.out.println(e[i].age);
+	       			System.out.println(e[i].salary);
+	       			System.out.println(e[i].cabin);
+	       		}          
+	                               	                               
 		}
-
-		
-		
-		
-		
-		
+				
+						
 	}
 	
 	
