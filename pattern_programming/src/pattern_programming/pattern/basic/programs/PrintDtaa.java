45746577a54ac3dfa208784f84e35142101a25e2
Yogesh kawade <ykawade99@gmail.com>
2022-10-16 09:35:53 +0530

    program

---
+++ pattern_programming/src/pattern_programming/pattern/basic/programs/PrintDtaa.java
@@ -0,0 +1,12 @@
+package pattern_programming.pattern.basic.programs;
+
+public class PrintDtaa {
+   
+	 public static void main(String []args ) {
+		 
+		 for(int i =10 ; i>=0; i--) {
+			 
+			 System.out.println(i);
+		 }
+	 }
+}
