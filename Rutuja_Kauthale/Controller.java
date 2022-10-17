581a11a68332409b983c8df031cf3eb9dc63944c
Rutuja <rutujakauthale28@gmail.com>
2022-10-17 16:05:59 +0530

    My java practice

---
+++ Rutuja_Kauthale/Controller.java
@@ -0,0 +1,19 @@
+import java.util.*;
+ class Controller
+{
+public static void main(String args[])
+{
+  	Student s=new Student();
+	s.rollnumber=10;
+	s.name="rutuja";
+	
+
+}
+}
+
+class Student{
+
+int rollnumber;
+String name;
+static String address;
+}
