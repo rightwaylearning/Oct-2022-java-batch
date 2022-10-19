d84a7adb97b5471700238870f255339b987264ff
Rutuja <rutujakauthale28@gmail.com>
2022-10-19 16:42:32 +0530

    file

---
+++ java-basic-programming-ch-1/src/nirmal_set_get/controller.java
@@ -0,0 +1,29 @@
+package nirmal_set_get;
+
+public class controller {
+
+	public static void main(String[] args) {
+
+		D d = new D(100, 200);
+		int obj1 = d.geta();
+		System.out.println(obj1);
+
+		int obj2 = d.getb();
+		System.out.println(obj2);
+		
+		
+		D d1 = new D();
+		d1.seta(500);
+		d1.setb(600);
+		
+		
+	      int a = d1.geta();
+	      System.out.println(a);
+		  int b = d1.getb();
+		  System.out.println(b);
+		  
+//		System.out.println(a);
+//		System.out.println(b);
+	}
+
+}
