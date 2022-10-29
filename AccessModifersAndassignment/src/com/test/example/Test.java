33a1606a4c6021c3cc9b656aafc1507c9a4d48eb
sachin lagad <sachin.lagad@gmail.com>
2022-10-29 20:48:26 +0530

    opt

---
+++ AccessModifersAndassignment/src/com/test/example/Test.java
@@ -0,0 +1,21 @@
+package com.test.example;
+
+class A {
+	public static void main(String[] args) {
+		System.out.println("I am A classs");
+	}
+}
+
+class C {
+
+}
+
+class B {
+	public static void main(String[] args) {
+		System.out.println("I am B classs");
+	}
+}
+
+class D {
+
+}
