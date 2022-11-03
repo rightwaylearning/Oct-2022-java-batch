c850516b96fec96b4a32ea2764fb4a9065304c67
SARTHAK <sarthaknalage@gmail.com>
2022-11-03 23:22:43 +0530

    PackageAccess within same and among differ pack

---
+++ AccessModifersAndassignment/src/sarthak_nalage/subPack2/Controller.java
@@ -0,0 +1,31 @@
+package sarthak_nalage.subPack2;
+
+import sarthak_nalage.subPack1.Test1;
+//import sarthak_nalage.subPack1.Controller;
+//import sarthak_nalage.subPack2.Test;
+import sarthak_nalage.subPack1.Test;
+
+ public class Controller {
+	public void c2(String name) {
+		System.out.println("welcome " + name);
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println("Trying to call methods inside subPack1");
+		Test1 t1 = new Test1();
+		Test t2 = new Test();// Here, Test class belongs to the subPack1
+		t2.t2();
+		// suppose we wan't to be cl method from the Test class of subPack2
+		sarthak_nalage.subPack2.Test t3 = new sarthak_nalage.subPack2.Test();
+		t3.t4();
+
+		// t1.t1(); C.E bcz of method define it's not public in case
+		t1.t1();// Now it's public so easily it's accessible
+		sarthak_nalage.subPack1.Controller c1 = new sarthak_nalage.subPack1.Controller();
+		c1.c1("Sarthak");// Hello Sarthak
+		Controller c2 = new Controller();
+		c2.c2("Sarthak");// welcome Sarthak
+
+	}
+}
---
+++ AccessModifersAndassignment/src/sarthak_nalage/subPack2/Test3.java
@@ -0,0 +1,8 @@
+package sarthak_nalage.subPack2;
+
+public class Test3 {
+	public void t3() {
+		System.out.println("Inside sarthak_nalage.subPack2.Test3" );
+	}
+
+}
