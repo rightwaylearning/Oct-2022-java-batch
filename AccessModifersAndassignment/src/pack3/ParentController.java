85e8967618af8fe15add23da7fcec0fb5e96a425
sachin lagad <sachin.lagad@gmail.com>
2022-11-03 20:04:58 +0530

    import

--- AccessModifersAndassignment/src/pack3/ParentController.java
+++ AccessModifersAndassignment/src/pack3/ParentController.java
@@ -1,17 +1,22 @@
 package pack3;
 
-
-import pack1.Controller;
+import java.sql.Date;
 
 public class ParentController {
 
 	public static void main(String[] args) {
 		
-		Controller c1 = new Controller();
+		pack1.Controller c1 = new pack1.Controller();
 		c1.main();
 		
 		pack2.Controller c2 =new pack2.Controller();
 		c2.main1();
+		
+		long l = 3638457638L;
+		Date d1 = new Date(l);
+		
+		java.util.Date d2 = new java.util.Date();
+		d2.getYear();
 		
 		
 	}
