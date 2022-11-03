d3d23ae06032964def91908c57472e610f2afc69
sachin lagad <sachin.lagad@gmail.com>
2022-11-03 20:02:44 +0530

    import

---
+++ AccessModifersAndassignment/src/pack1/Controller.java
@@ -0,0 +1,16 @@
+package pack1;
+
+public class Controller {
+
+	public void main() {
+		String str = new String("hello Good morning");
+		
+		A a= new A();
+		a.m1();
+		
+		B b= new B();
+		b.m2();
+		
+		System.out.println(str);
+	}
+}
