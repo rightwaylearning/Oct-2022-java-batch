6785dcb68833916d71e077280bdd5a635828522c
sachin lagad <sachin.lagad@gmail.com>
2022-11-10 10:07:57 +0530

    data

---
+++ SpringDemoExample/src/test/Controller.java
@@ -0,0 +1,15 @@
+package test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Controller {
+
+	public static void main(String[] args) {
+		
+		
+	
+		ApplicationContext  app = new ClassPathXmlApplicationContext("application.xml");
+	
+	}
+}
