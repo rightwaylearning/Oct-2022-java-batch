4111e2dd1153c1aba0e1e809d8b9165e4e647b3e
Nirmal Rajput <nirmal12796@gmail.com>
2022-11-15 10:44:28 +0530

    parent child relation

---
+++ abstract_class_And_Interface/src/Nirmal_child_parent_relation/Controller.java
@@ -0,0 +1,15 @@
+package Nirmal_child_parent_relation;
+
+public class Controller {
+	public static void main(String[] args) {
+		Customer customer = new Customer();
+
+		customer.burger();
+
+		customer.cofee();
+
+		customer.pizaa();
+
+	}
+
+}
