4e8df82cfeaede8b63f100e5e61dc5d27e8c19d8
Nirmal Rajput <nirmal12796@gmail.com>
2022-11-16 11:43:32 +0530

    this is my abtract practice

--- AccessModifersAndassignment/src/nirmal_abtract_practice/empcontroller.java
+++ AccessModifersAndassignment/src/nirmal_abtract_practice/empcontroller.java
@@ -5,10 +5,11 @@ public class empcontroller {
 	public static void main(String[] args) {
 
 		info i = new info();
+
 		System.out.println(i.firstname);
 		System.out.println(i.lastname);
 		System.out.println(i.id);
-		i.getEmployeeinfo("indra","rajput", 23);
+		i.getEmployeeinfo("indra", "rajput", 23);
 
 	}
 
