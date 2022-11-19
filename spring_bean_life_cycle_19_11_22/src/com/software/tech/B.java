bb58306d2f129a52a73fe7e7bebc6241e01fd7e8
sachin lagad <sachin.lagad@gmail.com>
2022-11-19 22:37:16 +0530

    msg

--- spring_bean_life_cycle_19_11_22/src/com/software/tech/B.java
+++ spring_bean_life_cycle_19_11_22/src/com/software/tech/B.java
@@ -1,28 +1,5 @@
 package com.software.tech;
 
 public class B {
-	Integer x;
-	Integer y;
-
-	public Integer getX() {
-		return x;
-	}
-
-	public void setX(Integer x) {
-		this.x = x;
-	}
-
-	public Integer getY() {
-		return y;
-	}
-
-	public void setY(Integer y) {
-		this.y = y;
-	}
-
-	@Override
-	public String toString() {
-		return "B [x=" + x + ", y=" + y + "]";
-	}
-
+	
 }
