c6211b90d3e40d051d385206495a40495a66cff4
Nirmal Rajput <nirmal12796@gmail.com>
2022-11-19 10:20:25 +0530

    inheritance and interface practice

---
+++ abstract_class_And_Interface/src/nirmal_inheritance_practice/Company.java
@@ -0,0 +1,14 @@
+package nirmal_inheritance_practice;
+
+ public class Company {
+	
+	 void compnaydetail() {
+		 String CompanyName = "infosys";
+		 int Companyproject = 750;
+		 
+		 System.out.println(CompanyName);
+		 System.out.println(Companyproject); 
+		 
+	 }
+
+}
