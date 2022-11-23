79267e061fe971fe116509687416c5dbfd05aac0
Archana khaire <archanakhaire1986@gmail.com>
2022-11-23 23:18:31 +0530

    accesmodifire

---
+++ Archana_kalkhaire_basic_pro/src/Archana_Thisdemo/Employee.java
@@ -0,0 +1,12 @@
+package Archana_Thisdemo;
+
+public class Employee {
+	int rollNumber;
+	String name;
+	
+	Employee(int rollNumber,String name){
+		this.rollNumber = rollNumber;
+		this.name = name;
+	}
+	
+}
