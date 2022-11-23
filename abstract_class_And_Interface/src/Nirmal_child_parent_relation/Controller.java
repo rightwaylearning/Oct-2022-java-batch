e3a0980cfb072b84dbd970ee77be11ea5e9c7e55
Nirmal Rajput <nirmal12796@gmail.com>
2022-11-23 23:14:13 +0530

    assisgment

--- abstract_class_And_Interface/src/Nirmal_child_parent_relation/Controller.java
+++ abstract_class_And_Interface/src/Nirmal_child_parent_relation/Controller.java
@@ -3,9 +3,13 @@ package Nirmal_child_parent_relation;
 public class Controller {
 	public static void main(String[] args) {
 		CafeMenuCard c = new CafeMenuCardIpl();
+//		c.burger();
 
-		Customer customer = new Customer();
-		customer.order(c);
+    	Customer customer = new Customer();
+    	customer.order(c);
+    	
+    	
+//    	
 
 	}
 
