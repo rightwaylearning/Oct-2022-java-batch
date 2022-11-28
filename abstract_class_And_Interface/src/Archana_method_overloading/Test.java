3ab6c01cf74f3ce6e74e9aba307a5acc9418f002
Archana khaire <archanakhaire1986@gmail.com>
2022-11-28 22:58:04 +0530

    overloding pro

---
+++ abstract_class_And_Interface/src/Archana_method_overloading/Test.java
@@ -0,0 +1,21 @@
+package Archana_method_overloading;
+
+public class Test {
+	
+	public void m1(int a,int b) {} //  m1(int int)
+	
+	
+	public void m1(int a,int b,int c) {}// m1(int int int)
+	
+	public void m1(float f,int a) {}// m1(float int)
+	
+	public void m1(int a,float f) {} // m1(int float)
+	
+	//method OverLoading  >>  1] method name should same
+                            // 2] parameter should be diff
+                            // 1] count, data type diff, sequence
+
+	
+	
+
+}
