0d7d670bb214a4ff7f995541fadd833824960d23
dhanashri.udar <udardhanshri2018@gmail.com>
2022-12-03 22:53:50 +0530

    hfhf

---
+++ abstract_class_And_Interface/src/Dhanu_method_overriding/EntryPoint.java
@@ -0,0 +1,39 @@
+package Dhanu_method_overriding;
+
+class A{}
+class B extends A{}
+class C1 extends B{}
+
+ class SouthGate{
+	
+	 SouthGate(){}
+	 
+	  B keyEntryPoint(int a) {
+		  System.out.println("SouthGate");
+		  return null;
+	  }
+}
+
+ class NorthGate extends SouthGate{
+	 
+	 NorthGate(){}
+    
+	 @Override
+	 C1 keyEntryPoint(int x) {
+		 System.out.println("NorthGate");
+		 return null;
+	 }
+}
+
+
+
+
+
+public class EntryPoint {
+
+	public static void main(String[] args) {
+	
+	}
+}
+
+
